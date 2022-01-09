36a54ee344d594f0d5adff1eb1f21ae1c7cc0058
Paul Horstmann <paulluishorstmann@gmail.com>
2022-01-09 15:09:25 +0100

    add jshint

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 String.prototype.toProperCase = function () {
-    return this.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+    return this.replace(
+        /\w\S*/g,
+        (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    );
 };
 
 String.prototype.toSentenceCase = function () {
-    return this.replace(/\b((?!=|\.).)+(.)/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+    return this.replace(
+        /\b((?!=|\.).)+(.)/g,
+        (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    );
 };
 
 function download(filename, text) {
     let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+    element.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+    );
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
@@ -18,15 +27,30 @@ function download(filename, text) {
 
 function buttonEventHandler(event) {
     switch (event.path[0].id) {
-        case ('upper-case'): textarea.value = textarea.value.toUpperCase(); break;
-        case ('lower-case'): textarea.value = textarea.value.toLowerCase(); break;
-        case ('proper-case'): textarea.value = textarea.value.toProperCase(); break;
-        case ('sentence-case'): textarea.value = textarea.value.toSentenceCase(); break;
-        case ('save-text-file'): download("text.txt", textarea.value); break;
-        default: alert('An error has occurred');
+        case 'upper-case':
+            textarea.value = textarea.value.toUpperCase();
+            break;
+        case 'lower-case':
+            textarea.value = textarea.value.toLowerCase();
+            break;
+        case 'proper-case':
+            textarea.value = textarea.value.toProperCase();
+            break;
+        case 'sentence-case':
+            textarea.value = textarea.value.toSentenceCase();
+            break;
+        case 'save-text-file':
+            download('text.txt', textarea.value);
+            break;
+        default:
+            alert('An error has occurred');
     }
 }
 
-const textarea = document.getElementById('inp-txtarea')
+const textarea = document.getElementById('inp-txtarea');
 
-document.querySelectorAll('button').forEach(node => node.addEventListener("click", (e) => buttonEventHandler(e)))
+document
+    .querySelectorAll('button')
+    .forEach((node) =>
+        node.addEventListener('click', (e) => buttonEventHandler(e))
+    );
